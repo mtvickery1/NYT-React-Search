6e03e4e752de7b1a00e9f3ceeaef9267ef5f9cce
mv04 <mtvickery1@gmail.com>
2018-09-24 21:46:32 -0500

    created header component

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Header from "./components/Header";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        Test
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Header = () => (
+    <div className="jumbotron bg-warning">
+  
+      <h1 className="text-center">
+      <i class="fa fa-newspaper-o" aria-hidden="true"></i> New York Times Search
+      </h1>
+  
+    </div>
+  );
+
+  export default Header;
